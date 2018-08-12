518ecd5f6129d88bf76123f9d5f11443f8718e30
pengpeng308182648 <18270820930@163.com>
2018-08-12 20:14:21 +0800

    数据结构学习

--- src/test/java/dataStructure/study/Study02Sort.java
+++ src/test/java/dataStructure/study/Study02Sort.java
@@ -9,22 +9,42 @@ public class Study02Sort {
     public static void main(String[] args) {
         int maxSize = 10;
         Array array = new Array(maxSize);
-        array.insert(77);
-        array.insert(77);
-        array.insert(29);
-        array.insert(29);
-        array.insert(7);
-        array.insert(29);
+        array.insert(7777);
+        array.insert(777);
+        array.insert(297);
+        array.insert(292);
+        array.insert(291);
+        array.insert(290);
         array.insert(234);
-        array.insert(57);
-        array.insert(70);
+        array.insert(157);
+        array.insert(156);
+        array.insert(157);
+        array.insert(154);
+        array.insert(153);
+        array.insert(152);
+        array.insert(151);
+        array.insert(150);
+        array.insert(149);
+        array.insert(148);
+        array.insert(147);
+        array.insert(146);
+        array.insert(145);
+        array.insert(144);
+        array.insert(143);
+        array.insert(142);
+        array.insert(141);
+        array.insert(140);
+        array.insert(73);
+        array.insert(71);
         array.insert(70);
         array.display();
         //array.bubbleSort();
         //array.selectSort();
         //array.insertSort();
         //array.bubbleSortDoubleEnd();
-        array.noDups();
+       //array.noDups();
+        //System.out.println("Times "+array.oddEvenSort());
+        System.out.println(Arrays.toString(array.insertSortFor()));
         array.display();
     }
 
@@ -37,6 +57,9 @@ class Array{
         size = 0;
     }
     public void insert(long value){
+        if (size == a.length){
+            a = Arrays.copyOf(a,size+1);
+        }
         a[size] = value;
         size++;
     }
@@ -152,6 +175,45 @@ class Array{
         }
     }
     /**
+     * 插入排序for
+     */
+    public int[] insertSortFor(){
+        int copy = 0;
+        int comp = 0;
+        int dups = 0;
+        for(int i=1;i<size;){
+            int j = i-1;
+            long key = a[i];
+            for (;j>=dups;j--){
+                if(a[j] == key){
+                    a[i] = -1;
+                    key = -1;
+                    dups++;
+                    break;
+                }
+            }
+            for (j=i-1;j>=0;j--){
+                comp++;
+                if(a[j] > key){
+                    a[j+1] =a[j];
+                    copy++;
+                }else {
+                    break;
+                }
+            }
+            a[j+1] = key;
+            copy++;
+            i++;
+        }
+        //数组前移
+        for(int i=dups;i<size;i++){
+            a[i-dups] = a[i];
+        }
+        size -= dups;
+        a = Arrays.copyOf(a,size);
+        return new int[]{comp,copy};
+    }
+    /**
      * 删除重复元素O(n)
      */
     public void noDups(){
@@ -166,5 +228,68 @@ class Array{
         }
         size -=dup;
         a = Arrays.copyOf(a,size);
+    }
+    /**
+     * 奇偶排序
+     */
+    public int oddEvenSort(){
+        boolean odd1 = false;
+        boolean even1 = false;
+        boolean odd2 = false;
+        boolean even2 = false;
+        boolean oddFlag = true;
+        int times = 0;
+        while (true){
+            if(oddFlag){
+                for(int i=size-1;i>0;i-=2){
+                    boolean didBub = false;
+                    for(int j=1;j<i;j+=2){
+                        if(a[j] > a[j+1]){
+                            swap(j,j+1);
+                            didBub = true;
+                        }
+                    }
+                    if (!didBub){
+                        if(odd1){
+                            odd2 = true;
+                        }else {
+                            odd2 = false;
+                        }
+                        odd1 = true;
+                        break;
+                    }else {
+                        odd1 = false;
+                    }
+                }
+                oddFlag = false;
+            }else{
+                for(int i=size-1;i>0;i-=2){
+                    boolean didBub = false;
+                    for(int j=0;j<i;j+=2){
+                        if(a[j] > a[j+1]){
+                            swap(j,j+1);
+                            didBub = true;
+                        }
+                    }
+                    if (!didBub){
+                        if(even1){
+                            even2 = true;
+                        }else{
+                            even2 = false;
+                        }
+                        even1 = true;
+                        break;
+                    }else {
+                        even1 = false;
+                    }
+                }
+                oddFlag = true;
+            }
+            if(odd2 & even2){
+                break;
+            }
+            times++;
+        }
+        return times;
     }
 }
